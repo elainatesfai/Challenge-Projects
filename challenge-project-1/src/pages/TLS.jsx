65da15a1f4f489cc1b6ce0d9cc9dada7f854de2c
FilipaChambel <vivi.chambel@gmail.com>
2022-12-22 15:03:01 +0000

    Created Button

--- challenge-project-1/src/pages/TLS.jsx
+++ challenge-project-1/src/pages/TLS.jsx
@@ -4,8 +4,9 @@ import Navbar from '../components/Navbar'
 function TLS() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       THIS IS TRAFFIC LIGHT SYSTEM
+      <button className="button Start">Start Probe</button>
     </>
   )
 }
